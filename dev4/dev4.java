db7e2d5db1f96331537a4bb2d0d6f9af40677154
jerem <jeremie.cyrille@icloud.com>
2020-04-14 11:43:16 -0400

    1st comit

---
+++ dev4/dev4.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+//******************************************************************** //
+// Devoir4.java     Auteur: Gansonre Ismael && jeremie cyrille
+// // 
+//******************************************************************
+
+enum Couleur {ROUGE, JAUNE, VERT};
+class Voiture implements Comparable <Voiture> {
+    String Marque;
+    int Annnee = 0;
+    Couleur color;
+    int compteur = 0;
+    Voiture(String Marque,int Annnee,String color){
+         this.Marque=Marque;
+         this.Annnee=Annnee;
+         this.color=Couleur.valueOf(color);
+         compteur++;
+         }
+         public int compareTo(Voiture vt){  
+            if(Annnee==vt.Annnee)  
+            return 0;  
+            else if(Annnee>vt.Annnee)  
+            return 1;  
+            else  
+            return -1;  
+            }  
+    public String toString() {
+        return "Voiture{"+"entry= "+compteur +"Marque= " + Marque + '\'' + ", Annnee= " + Annnee +'}';
+    }
+}    
+public class dev4{
+public static int main(String[] args) {
+    ArrayList<Voiture>test=new ArrayList<Voiture>();
+    test.add(new Voiture(Marque, Annnee, color));
+    test.remove(Voiture());
+
+}
+}
